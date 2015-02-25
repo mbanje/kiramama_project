1ff824fb06bbdd12a4061ff1335c7d2a55282c33
mbanje <mbanjejvernack@gmail.com>
2015-02-25 14:01:18 +0200

    First four models synced

---
+++ kiramama_project/localsettings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kiramama',
+        'USER': 'mbanje',
+        'PASSWORD': 'nele2691988',
+        #'HOST': 'localhost',
+        'PORT': '',
+    }
+}
